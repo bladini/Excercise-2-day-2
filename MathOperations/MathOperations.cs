499318517eb50b409da4d1c25eff13ede563e616
Dmitriy Elisov <dmitry.elisov@phystech.edu>
2015-03-30 02:01:59 +0300

    Initial commit

---
+++ MathOperations/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class MathOperations
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
